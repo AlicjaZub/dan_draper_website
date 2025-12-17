2e20bae13de7bd25b21fdb9ac54e154111a703c1
AlicjaZub <alicja@mail.com>
2025-12-17 09:26:59 +0000

    Project card styling

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -56,7 +56,7 @@ const ProjectCard = ({ isOpen, onClose, id, type }: any) => {
           duration: 2.0,
           ease: [0.16, 1, 0.3, 1],
         }}
-        className="bg-black rounded-2xl shadow-xl p-6 relative z-50"
+        className="bg-black rounded-2xl shadow-xl p-6 relative z-50 m-6 mt-12"
         onClick={(e) => e.stopPropagation()}
       >
         <button
